d7fba8ad37cae0c9c50cc788be56551a977334bd
unknown <anailys1991@gmail.com>
2021-02-23 20:02:41 +0100

    funciones cabeceras define

---
+++ intro_c/cabecera.h
@@ -0,0 +1,8 @@
+#ifndef CABECERA_H
+#define CABECERA_H
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h> //libreria del sistema system("PAUSE")
+
+#endif // CABECERA_H
--- intro_c/main.cpp
+++ intro_c/main.cpp
@@ -1,13 +1,28 @@
 #include <QCoreApplication>
-#include <stdio.h>
-#include <iostream>
-#include <stdlib.h> //libreria del sistema system("PAUSE")
+#include "cabecera.h"
+
+#define suma(a,b) a+b
 
 using namespace std;
 
 int num = 30;
 int num2;
 bool comparador;
+
+int funcion (int a,int b)
+{
+
+    return a%b;
+}
+
+struct contacto
+{
+    int tel;
+    int edad;
+
+};
+
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -51,6 +66,26 @@ comparador=(n1<n4 && n3<n2); //conque se cumplan todas las condiciones
  comparador=(n1<n4  || n3<n2); //con que cumpla una de las condiciones
   cout << comparador << endl;
 
+//invirte y asigna el resultado
+
+ comparador=!comparador;
+  cout <<"el inverso de laoperacion anterior es "<< comparador << endl;
+
+//uso del define analogo a utilizar una sustitución o una macro
+
+  cout <<"la suma es "<< suma(1,20) << endl;
+
+ //ejemplo de funciones
+  cout << "operador modulo el resulatado entre " <<n2<<" y "<<n1<<"\n"<<funcion(n2,n1)<< endl;
+
+  //estructura
+contacto ana;
+ana.tel=205734;
+ana.edad=29;
+
+cout<< "el telefono de ana es "<<ana.tel<<endl;
+
 
     return a.exec();
+
 }
